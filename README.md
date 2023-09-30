29b5974dd35c82947c2388b39f13cd7e813c1ee5
MurphyWhite <mzh306291465@gmail.com>
2023-09-30 15:17:55 +0900

    添加脚本

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PZRSegment
+Segment是基于jieba分词做的一个Mac OS下的小应用
+
+## 依赖
+1. 依赖了homebrew进行python和git的安装
+2. 依赖pip安装python所需要的依赖包
+
+## 如何运行
+1. 点击update.sh安装依赖
+2. 点击run.sh将origin目录的txt文件进行分词后，保存在output目录
--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./python-3.x.x-macosx10.x/bin/python main.py
+# 获取当前脚本所在的目录
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# 切换到脚本所在的目录
+cd "$SCRIPT_DIR"
+
+# 运行 main.py
+echo "准备运行, 请将需要分词的语句放入txt文件，并放入origin文件夹中，按回车键继续"
+read -r
+python3 main.py
+echo "运行完成"
